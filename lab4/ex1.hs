d6e5a0548c3463b8213bd689eb906c5768e127e3
Kasper Sapała <kasper.sapala@gmail.com>
2018-11-23 14:54:16 +0100

    Lab4 init

---
+++ lab4/ex1.hs
@@ -0,0 +1,35 @@
+polarToCartesian :: Floating a => (a,a) -> (a,a)
+polarToCartesian (r,phi) = (r * cos phi, r * sin phi)
+
+type CartesianCoord' a = (a,a)
+type PolarCoord' a = (a,a)
+
+polarToCartesian' :: Floating a => PolarCoord' a -> CartesianCoord' a
+polarToCartesian' (r,phi) = (r * cos phi, r * sin phi)
+
+newtype CartesianCoord'' a = MkCartesianCoord'' (a,a)
+newtype PolarCoord'' a = MkPolarCoord'' (a,a)
+
+polarToCartesian'' :: Floating a => PolarCoord'' a -> CartesianCoord'' a
+polarToCartesian'' (MkPolarCoord'' (r,phi)) = MkCartesianCoord'' (r * cos phi, r * sin phi)
+
+
+personInfoToString :: (String,String,String) -> String
+personInfoToString (nm,snm,addr) =
+ "name: " ++ nm ++ ", surname: " ++ snm ++ ", addr: " ++ addr
+
+
+type Name' = String
+type Surname' = String
+type Address' = String
+type PersonInfo' = (Name', Surname', Address')
+type PersonInfoToStringType' = PersonInfo' -> String
+
+personInfoToString' :: PersonInfoToStringType'
+personInfoToString' (n, s, a) = "name: " ++ n ++ ", surname: " ++ s ++ ", addr: " ++ a
+
+
+newtype PersonInfo''  = MKPersonInfo'' (String, String, String)
+
+personInfoToString'' :: PersonInfo'' -> String
+personInfoToString'' (MKPersonInfo''(nm, snm, addr)) = "name: " ++ nm ++ ", surname: " ++ snm ++ ", addr: " ++ addr
---
+++ lab4/ex2.hs
@@ -0,0 +1,74 @@
+-- product type example (one constructor)
+data CartInt2DVec = MkCartInt2DVec Int Int -- konwencja: prefix 'Mk' dla konstruktora
+
+xCoord :: CartInt2DVec -> Int
+xCoord (MkCartInt2DVec x _) = x
+
+yCoord :: CartInt2DVec -> Int
+yCoord (MkCartInt2DVec _ y) = y
+
+data Cart2DVec' a = MkCart2DVec' a a
+
+xCoord' :: Cart2DVec' a -> a
+xCoord' (MkCart2DVec' x _) = x
+
+yCoord' :: Cart2DVec' a -> a
+yCoord' (MkCart2DVec' _ y) = y
+
+data Cart2DVec'' a = MkCart2DVec'' {x::a, y::a}
+
+
+-- sum type example (two constructors)
+data List a = EmptyL | Cons a (List a) deriving Show
+
+head' :: List a -> a
+head' EmptyL      = error "head': the empty list has no head!"
+head' (Cons x xs) = x
+
+-- enum type example (special case of sum type)
+data ThreeColors = Blue |
+                   White |
+                   Red
+
+type ActorName = String
+
+leadingActor :: ThreeColors -> ActorName
+leadingActor Blue  = "Juliette Binoche"
+leadingActor White = "Zbigniew Zamachowski"
+leadingActor Red   = "Irene Jacob"
+
+
+{-
+uwaga: ta sama nazwa* dla:
+ - konstruktora typu (po lewej)
+ - konstruktora danych/wartości (po prawej)
+
+ * druga (obok omówionej poprzednio -- z prefiksem 'Mk') powszechna konwencja w Haskellu!
+-}
+type Cart3DVec a = (a, a, a)
+-- let p123 = (1,2,3) :: Cart3DVec Int
+
+xCoord3D :: Cart3DVec a -> a
+xCoord3D (x, _, _) = x
+
+yCoord3D :: Cart3DVec a -> a
+yCoord3D (_, y, _) = y
+
+zCoord3D :: Cart3DVec a -> a
+zCoord3D (_, _, z) = z
+
+data Cart3DVec' a = MkCart3DVec' {cartX::a, cartY::a, cartZ::a }
+
+data Shape = Circle Float |
+             Rectangle Float Float
+area :: Shape -> Float
+area (Circle r) = 3.14*r^2
+area (Rectangle a b) = a*b
+
+data Tree a = EmptyT |
+              Node a (Tree a) (Tree a)
+              deriving Show
+
+rootValue :: Tree a -> a
+rootValue EmptyT = error "drzewo puste"
+rootValue (Node n lt rt) = n 
